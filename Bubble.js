692a6f4f5c37137896b520230b84467ff5bf6060
AbdulHafizM <67843140+AbdulHafizM@users.noreply.github.com>
2020-07-05 01:23:51 +0100

    Update Bubble.js

--- Bubble.js
+++ Bubble.js
@@ -1,6 +1,12 @@
-let arr = [1,6,5,2,3];
- for(let i = 0; i<arr.length;i++){
-   for(let j = 1; j<arr.length; j++){
-       if(
-}
-}
+    let arr = [2,10,8,5,9];
+    for(let i = 0; i<arr.length; i++){
+       for(let j = 0; j<arr.length; j++){
+          if(arr[j]>arr[j + 1]){
+             let temp = arr[j];
+             console.log(temp)
+             arr[j] = arr[j+1];
+             arr[j+1] = temp;
+          }
+       }
+    }
+    console.log(arr);
